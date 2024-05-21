fc3f528e0ae0fcd5568099860697547056d1f7f9
Antonius Riki Hermawan <rikihermawan9342@gmail.com>
2024-05-21 15:49:40 +0700

    menambahkan latian pertemuan 7

--- PemrogramanObjek5/src/main/java/PBO7/ReciptRevisited.java
+++ PemrogramanObjek5/src/main/java/PBO7/ReciptRevisited.java
@@ -12,20 +12,26 @@ import java.io.PrintWriter;
  */
 public class ReciptRevisited {
     public static void main(String[] args) throws Exception {
-        PrintWriter fileout = new PrintWriter("receipt.txt");
+        //PrintWriter fileout = new PrintWriter("receipt.txt");
         
-                fileout pritnln("+------------------------+");
-                fileout println("|                        |");
-                fileout println("|     DATA MAHASISWA     |");
-                fileout println("|                        |");
-                fileout println("| Antonius Riki Hermawan |");
-                fileout println("|     NIM : 23201115     |");
-                fileout println("|       Umur : 20        |");
-                fileout println("|  Tinggi Badan : 169.4  |");
-                fileout println("|    Berat Badan : 49    |");
-                fileout println("|                        |");
-                fileout println("|------------------------|");
-                fileout.close();
+//                fileout pritnln("+------------------------+");
+//                fileout println("|                        |");
+//                fileout println("|     DATA MAHASISWA     |");
+//                fileout println("|                        |");
+//                fileout println("| Antonius Riki Hermawan |");
+//                fileout println("|     NIM : 23201115     |");
+//                fileout println("|       Umur : 20        |");
+//                fileout println("|  Tinggi Badan : 169.4  |");
+//                fileout println("|    Berat Badan : 49    |");
+//                fileout println("|                        |");
+//                fileout println("|------------------------|");
+//                fileout.close();
+    
+
+    //private static class fileout {
+
+       
+        
     }
     
 }
